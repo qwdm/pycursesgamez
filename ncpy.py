d80c790d7c9fe9081b05afef02863c907d1b1262
user <mixal4enko@gmail.com>
2014-08-02 17:29:05 +0400

    bunnies

--- ncpy.py
+++ ncpy.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 import curses
 import time
+import random
 
 from drawboard import drawboard
 
 BOARD = []
+BUNNY_EATEN = False
 
 class GameOver(Exception):
     pass
 
 class Snake(object):
-    def __init__(self, screen, init_len=7, ch=ord('0')):
+    def __init__(self, screen, init_len=7, ch=ord('0'), grow_rate=3):
         self.pieces = []
         self.screen = screen
         self.ch = ch
         self.vel = (0,0)
+        self.grow_rate = grow_rate
 
         maxY, maxX = screen.getmaxyx()
         centY, centX = maxY//2, maxX//2
@@ -25,6 +28,7 @@ class Snake(object):
     def draw(self):
         for p in self.pieces:
             self.screen.addch(p[0], p[1], self.ch)
+        self.screen.move(0,0)
 
     def turn(self, direction):
         directions = {
@@ -35,18 +39,63 @@ class Snake(object):
         }
         self.vel = directions[direction]
 
+    def set_bunny(self, bunny):
+        self.bunny = bunny
+
     def move(self):
+        bunny_eaten = False
         head = self.pieces[0]
         tail = self.pieces[-1]
-        self.screen.addch(tail[0], tail[1], ord(' '))
+
         newhead = head[0] + self.vel[0], head[1] + self.vel[1]
+
         if newhead in self.pieces or newhead in BOARD:
             raise GameOver
-        self.pieces = [newhead] + self.pieces[:-1]
+
+        if newhead == self.bunny.coord():
+            self.isgrowing += self.grow_rate
+            bunny_eaten = True
+
+        if not self.isgrowing:
+            self.screen.addch(tail[0], tail[1], ord(' '))
+            self.pieces = [newhead] + self.pieces[:-1]
+        else:
+            self.pieces = [newhead] + self.pieces
+            self.isgrowing -= 1
+
+        if bunny_eaten:
+            self.bunny.new(self.pieces)
 
     def start(self):
+        self.isgrowing = 0
+        self.bunny.new(self.pieces)
+        self.draw()
         self.screen.getch()
         self.turn('right')
+
+
+class Bunny(object):
+    def __init__(self, screen):
+        self.screen = screen
+
+    def new(self, forbidden):
+        maxY, maxX = self.screen.getmaxyx()
+
+        def inside_board_rand(maxSmth):
+            return int(random.random() * (maxSmth-2)) + 1
+
+        while True:
+            self.x = inside_board_rand(maxX) 
+            self.y = inside_board_rand(maxY)
+            if (self.y, self.x) in forbidden:
+                continue
+            else:
+                break
+
+        self.screen.addch(self.y, self.x, ord('$'))
+
+    def coord(self):
+        return self.y, self.x
 
 
 class Screen(object):
@@ -80,12 +129,16 @@ def InitPoint(screen):
 
 def keyloop(stdscr):
     try:
+        bunny = Bunny(stdscr)
+        snake = Snake(stdscr)
+        snake.set_bunny(bunny)
+
         stdscr.nodelay(0)
         stdscr.clear()
+
         global BOARD
         BOARD = drawboard(stdscr)
-        snake = Snake(stdscr)
-        Point = InitPoint(stdscr)
+#        Point = InitPoint(stdscr)
         stdscr.refresh()
         stdscr.move(0,0)
         snake.start()
